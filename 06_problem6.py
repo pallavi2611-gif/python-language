21601193a55394af7c609a56e64fd9b489dad965
Pallavi Jadoun <pallavijadoun26@gmail.com>
2026-07-01 09:05:37 +0530

    Add files via upload

---
+++ 06_problem6.py
@@ -0,0 +1,4 @@
+a = int(input("Enter your number:"))
+
+print("The square of the number is:", a**2)
+# print("The square of the number is:", a*^2) incorrect for finding the square of a number in Python 
